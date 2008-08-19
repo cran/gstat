159a04d859e608c2625507b6e212b476f7592fe8
Edzer J. Pebesma <edzer.pebesma@uni-muenster.de>
2008-08-19 13:29:59 +0000

    version 0.9-48

--- R/variogramLine.R
+++ R/variogramLine.R
@@ -1,22 +1,24 @@
-# $Id: variogramLine.q,v 1.3 2006-10-31 14:36:55 edzer Exp $
+# $Id: variogramLine.q,v 1.4 2008-08-18 16:32:42 edzer Exp $
 
 "variogramLine" <-
-function(object, maxdist, n=200, min=1.0e-6 * maxdist, dir = c(1,0,0), 
-	covariance = FALSE,	..., debug.level = 0)
+function(object, maxdist, n = 200, min=1.0e-6 * maxdist, dir = c(1,0,0), 
+	covariance = FALSE,	..., dist_vector = numeric(0), debug.level = 0)
 {
 	if (missing(object))
 		stop("model is missing");
 	if (!inherits(object, "variogramModel"))
 		stop("model should be of mode variogramModel (use function vgm)")
-	if (missing(maxdist))
-		stop("maxdist is missing");
+	if (length(dist_vector) > 0)
+		maxdist = 0.0
+	else if (missing(maxdist))
+		stop("maxdist or dist_vector needs to be set");
 	if (length(dir) != 3)
 		stop("dir should be numeric vector of length 3")
 	.Call("gstat_init", as.integer(debug.level))
 	pars = c(min,maxdist,n,dir)
 	load.variogram.model(object, c(0,0)) # loads object into gstat 
 	ret = .Call("gstat_variogram_values", as.integer(c(0,0)),
-		as.numeric(pars), as.integer(covariance))
+		as.numeric(pars), as.integer(covariance), as.numeric(dist_vector))
 	.Call("gstat_exit", 0);
 	data.frame(dist=ret[[1]], gamma=ret[[2]])
 }
--- src/spbkp.c
+++ src/spbkp.c
@@ -31,7 +31,7 @@
   Radical revision completed Mon 07th Dec 1992, 10:59:57 AM
 */
 
-static	char	rcsid[] = "$Id: spbkp.c,v 1.1.1.1 2003-06-23 18:31:55 cees Exp $";
+static	char	rcsid[] = "$Id: spbkp.c,v 1.2 2008-05-19 09:58:07 edzer Exp $";
 
 #include	<stdio.h>
 #include        "sparse2.h"
