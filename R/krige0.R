10dcdc78e1e0db4714260b9c9aa2b142eb23dd7e
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2012-08-31 00:00:00 +0000

    version 1.0-14

--- R/krige0.R
+++ R/krige0.R
@@ -26,7 +26,8 @@ idw0 = function(formula, data, newdata, y) {
 
 CHsolve = function(A, b) {
 	# solves A x = b for x if A is PD symmetric
-	A = chol(A, LINPACK=TRUE)
+	#A = chol(A, LINPACK=TRUE)
+	A = chol(A)
 	backsolve(A, forwardsolve(A, b, upper.tri = TRUE, transpose = TRUE))
 }
 
@@ -135,8 +136,10 @@ STsolve = function(A, b, X) {
 # solve for L: use Y = L T 
 # S Y = c0 -> Y = solve(S, c0)
 # L T = Y -> Tt Lt = Yt -> Lt = solve(Tt, Yt)
-	Tm = chol(A$Tm, LINPACK=TRUE)
-	Sm = chol(A$Sm, LINPACK=TRUE)
+	#Tm = chol(A$Tm, LINPACK=TRUE)
+	Tm = chol(A$Tm)
+	#Sm = chol(A$Sm, LINPACK=TRUE)
+	Sm = chol(A$Sm)
 	STbacksolve = function(Tm, Cm, Sm) { 
 		MyChSolve = function(A, b)
 			backsolve(A, forwardsolve(A, b, upper.tri = TRUE, transpose = TRUE))
